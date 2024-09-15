6ce47e43fbd456a74e0ae15df00a351f1a295c2c
Andres <andrespodadera87@gmail.com>
2024-09-15 19:42:14 +0200

    Spring API using JdbcTemplate

---
+++ SpringDataSQL/footballpg/src/test/java/com/andres/footballpg/FootballpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.footballpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
